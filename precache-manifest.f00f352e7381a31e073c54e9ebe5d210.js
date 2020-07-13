1cbc4188c44addca1ecf89e03cd99e8ee9908f15
Magdalena Pietruszewska <magdalena-pietruszewska@wp.pl>
2020-07-13 14:06:46 +0200

    Updates

---
+++ precache-manifest.f00f352e7381a31e073c54e9ebe5d210.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e275b0ea9cd69f78cf10ee4489ef8910",
+    "url": "/InGoodHands/index.html"
+  },
+  {
+    "revision": "feabc81100310ec09d93",
+    "url": "/InGoodHands/static/css/main.a612992c.chunk.css"
+  },
+  {
+    "revision": "c16979d87d69d2679d9a",
+    "url": "/InGoodHands/static/js/2.7a03536d.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/InGoodHands/static/js/2.7a03536d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feabc81100310ec09d93",
+    "url": "/InGoodHands/static/js/main.2206398d.chunk.js"
+  },
+  {
+    "revision": "e8a61a11ce341580c68d",
+    "url": "/InGoodHands/static/js/runtime-main.133d1a57.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/InGoodHands/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/InGoodHands/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/InGoodHands/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/InGoodHands/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/InGoodHands/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/InGoodHands/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/InGoodHands/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/InGoodHands/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/InGoodHands/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/InGoodHands/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/InGoodHands/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/InGoodHands/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/InGoodHands/static/media/Signature.e54cfcfc.svg"
+  }
+]);
